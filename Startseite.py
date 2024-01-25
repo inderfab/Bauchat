e49a9b99e04d06793b6f679e625cdb442687e4e6
inderfab <93277338+inderfab@users.noreply.github.com>
2024-01-25 17:10:26 +0100

    Update Startseite.py

--- Startseite.py
+++ Startseite.py
@@ -29,7 +29,6 @@ if st.session_state["preload_data_loaded"] == False:
 st.session_state["data_user"] = None
 if st.session_state.username != "Temp": 
     db.load_data_user()
-    st.write("Folders", st.session_state.u_folders)
     st.session_state["u_collections"] = [n["collection"] for n in st.session_state["u_folders"]["collections"]]
     
 st.subheader("Laden sie ihre PDF-Dokumente hoch oder suchen Sie in den Verzeichnissen")
