611e22ede8ff625ceab40c1b6a09f9fb1ab6bfd8
Fabio Indergand <info@bauchat.ch>
2024-02-03 18:12:58 +0100

    css

--- configuration.py
+++ configuration.py
@@ -100,15 +100,9 @@ def conf_session_state():
 
 def conf_menu():
     css = """
-    .css-1jc7ptx, .e1ewe7hr3, 
-    .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, 
-    .viewerBadge_link__1S137, 
-    .viewerBadge_text__1JaDK{ display: none; } 
-    #MainMenu{ visibility: hidden; } 
-    footer { visibility: hidden; } 
-    header { visibility: hidden; }
-
+    #MainMenu {visibility: hidden;}
+    footer {visibility: hidden;}
+    #GithubIcon {visibility: hidden;}
     
     <style>
  
