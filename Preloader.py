c612d31772ff9f7f791082eb7bc2c60664d4e09d
Fabio Indergand <info@bauchat.ch>
2024-02-11 15:46:42 +0100

    df

--- Preloader.py
+++ Preloader.py
@@ -128,17 +128,19 @@ with st.container():
             #except:
             #    tags = ""
             st.write(collection["collection"].upper()) #, "      Tags: ",tags
-            edited_df = st.experimental_data_editor(collection["filenames"],
-                        use_container_width = True,
-                        column_order=("name","titel","sprache","num_pages","link","herausgabedatum","up_date"),
-                        column_config={"name":"Name",
-                                       "titel": "Titel",
-                                        "sprache": "Sprache",
-                                        "num_pages": "Seitenzahl",
-                                        "link": "Link",
-                                        "herausgabedatum": "Herausgabedatum",
-                                        "up_date": "Hochgeladen am"
-                                        },
-                        hide_index=True,
-                        )
+            df = collection["filenames"]
+            st.write(df)
+            # edited_df = st.experimental_data_editor(df,
+            #             use_container_width = True,
+            #             column_order=("name","titel","sprache","num_pages","link","herausgabedatum","up_date"),
+            #             column_config={"name":"Name",
+            #                            "titel": "Titel",
+            #                             "sprache": "Sprache",
+            #                             "num_pages": "Seitenzahl",
+            #                             "link": "Link",
+            #                             "herausgabedatum": "Herausgabedatum",
+            #                             "up_date": "Hochgeladen am"
+            #                             },
+            #             hide_index=True,
+            #             )
             
