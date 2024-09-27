60af36811e807d7c398d3011ad9525b1edad4466
inderfab <93277338+inderfab@users.noreply.github.com>
2024-09-27 12:32:58 +0200

    Update store.py

--- store.py
+++ store.py
@@ -15,10 +15,10 @@ import uuid
 
 st.session_state.update(st.session_state)
 
-AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
-AWS_DEFAULT_REGION = os.getenv("AWS_DEFAULT_REGION")
-AWS_BUCKET_NAME = os.getenv("AWS_BUCKET_NAME")
+AWS_ACCESS_KEY_ID = os.getenv(AWS_ACCESS_KEY_ID)
+AWS_SECRET_ACCESS_KEY = os.getenv(AWS_SECRET_ACCESS_KEY)
+AWS_DEFAULT_REGION = os.getenv(AWS_DEFAULT_REGION)
+AWS_BUCKET_NAME = os.getenv(AWS_BUCKET_NAME)
 
 # @st.cache_data
 # def load_data_preloaded():
