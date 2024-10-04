5ceaaf8590545e373b69ea3f0f6d59edb80dd1a6
inderfab <93277338+inderfab@users.noreply.github.com>
2024-10-04 13:17:13 +0200

    Update Startseite.py

--- Startseite.py
+++ Startseite.py
@@ -1,9 +1,13 @@
 import streamlit as st
-
+import os
 
 page_title = "bauCHAT"
 
 st.set_page_config(page_title = page_title,layout="wide") #
 
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_DEFAULT_REGION = os.getenv('AWS_DEFAULT_REGION')
+AWS_BUCKET_NAME = os.getenv('AWS_BUCKET_NAME')
 
-st.write("Das ist ein Test ob es läuft")
+st.write(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY,AWS_DEFAULT_REGION,AWS_BUCKET_NAME)
