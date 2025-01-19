3816bca0075ef2ef3555bdad4aeb947eaf49bec3
Fabio Indergand <info@bauchat.ch>
2025-01-19 12:07:47 +0100

    preloader

--- Preloader.py
+++ Preloader.py
@@ -66,7 +66,10 @@ st.session_state["username"] = key
 stream = st.file_uploader(label="Laden sie ihr PDF hoch oder suchen Sie in den Verzeichnissen", type='pdf',accept_multiple_files=True, label_visibility="hidden")
 
 if st.session_state["firmas"] == None:
-    db.fetch_all_firmas()
+    #db.fetch_all_firmas()
+    db.get_firmas() 
+
+
 
 c1,c2 = st.columns(2)
 with c1:
@@ -77,66 +80,66 @@ with c1:
     
     st.write("Firmas: ", st.session_state["firmas"])
 
-#     firma_id = st.selectbox(label="Firma wählen",options=[f["firma_name"]+" | "+f["key"] for f in st.session_state["firmas"]], index=None)
-#     if firma_id is not None:
-#         firma_id = firma_id.split(" | ")[1]
-#     link = st.text_input("link")
-#     tags = st.text_input("tags")
+    firma_id = st.selectbox(label="Firma wählen",options=[f["firma_name"]+" | "+f["key"] for f in st.session_state["firmas"]], index=None)
+    if firma_id is not None:
+        firma_id = firma_id.split(" | ")[1]
+    link = st.text_input("link")
+    tags = st.text_input("tags")
 
-#     st.session_state["metadata_preloaded"] = {"name":name,
-#                                               "language":sprache,
-#                                               "printdate":herausgabedatum,
-#                                               "firma_id":firma_id,
-#                                               "link":link,
-#                                               "tags":[t.strip()for t in tags.split(",")]
-#                                               }
+    st.session_state["metadata_preloaded"] = {"name":name,
+                                              "language":sprache,
+                                              "printdate":herausgabedatum,
+                                              "firma_id":firma_id,
+                                              "link":link,
+                                              "tags":[t.strip()for t in tags.split(",")]
+                                              }
 
-# with c2:
-#     firma_name = st.text_input("Firma Namen")
-#     firma_strasse = st.text_input("Firma Strasse")
-#     firma_plz = st.text_input("Firma PLZ")
-#     firma_ort = st.text_input("Firma Ort")
-#     firma_tel = st.text_input("Firma Tel")
-#     firma = {"firma_name":firma_name,"strasse":firma_strasse,"plz":firma_plz,"ort":firma_ort,"firma_tel":firma_tel}
-#     c_name= st.text_input("Kontakt Name")
-#     c_email = st.text_input("Kontakt Mail")
-#     c_tel = st.text_input("Kontakt Telefon")
-#     contact = {"contact_name:":c_name,"contact_email":c_email,"contact_tel":c_tel}
-#     firma.update(contact)
+with c2:
+    firma_name = st.text_input("Firma Namen")
+    firma_strasse = st.text_input("Firma Strasse")
+    firma_plz = st.text_input("Firma PLZ")
+    firma_ort = st.text_input("Firma Ort")
+    firma_tel = st.text_input("Firma Tel")
+    firma = {"firma_name":firma_name,"strasse":firma_strasse,"plz":firma_plz,"ort":firma_ort,"firma_tel":firma_tel}
+    c_name= st.text_input("Kontakt Name")
+    c_email = st.text_input("Kontakt Mail")
+    c_tel = st.text_input("Kontakt Telefon")
+    contact = {"contact_name:":c_name,"contact_email":c_email,"contact_tel":c_tel}
+    firma.update(contact)
 
-#     if st.button("Firma speichern"):
-#         st.write(db.insert_firma(firma))
-#         st.session_state["firmas"] == None
-#         st.session_state["firmas"] = None
-#         db.fetch_all_firmas()
-
-
+    if st.button("Firma speichern"):
+        st.write(db.insert_firma(firma))
+        st.session_state["firmas"] == None
+        st.session_state["firmas"] = None
+        db.fetch_all_firmas()
 
 
-# if stream != []:
-#     st.session_state["Files_Saved"] == False
-#     st.write("Key: ",st.session_state["preload_key"])
-#     db.load_data_user(st.session_state["preload_key"])
 
-#     st.session_state["speicher_expander"] = True
+
+if stream != []:
+    st.session_state["Files_Saved"] == False
+    st.write("Key: ",st.session_state["preload_key"])
+    db.load_data_user(st.session_state["preload_key"])
+
+    st.session_state["speicher_expander"] = True
     
-#     with st.expander("Sammlung erstellen", expanded=True):
+    with st.expander("Sammlung erstellen", expanded=True):
             
-#         sc1, sc2 = st.columns(2)
+        sc1, sc2 = st.columns(2)
         
-#         with sc1:
-#             collection = st.text_input("Neue Sammlung anlegen:", max_chars=30, help="maximal 25 Buchstaben", value=None)                        
-#             if collection is not None:
-#                 st.session_state["collection"] = collection
+        with sc1:
+            collection = st.text_input("Neue Sammlung anlegen:", max_chars=30, help="maximal 25 Buchstaben", value=None)                        
+            if collection is not None:
+                st.session_state["collection"] = collection
 
-#         with sc2:
-#             update_collection = st.selectbox('Sammlung aktualisieren',[n["collection"] for n in st.session_state["u_folders"]["collections"]], index=None)
-#             if update_collection is not None:
-#                 st.session_state["collection"] = update_collection
+        with sc2:
+            update_collection = st.selectbox('Sammlung aktualisieren',[n["collection"] for n in st.session_state["u_folders"]["collections"]], index=None)
+            if update_collection is not None:
+                st.session_state["collection"] = update_collection
         
-#         if st.button(label="Dokument speichern"):
-#             ai.submit_upload(stream)
-#             #st.form_submit_button("Speichern", on_click=ai.submit_upload, args=[stream])
+        if st.button(label="Dokument speichern"):
+            ai.submit_upload(stream)
+            #st.form_submit_button("Speichern", on_click=ai.submit_upload, args=[stream])
 
 ## Example usage:
 ##random_id = generate_id()
